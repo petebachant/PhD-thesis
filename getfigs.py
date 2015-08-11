d10b1978ff66c8319ef179f7ece5d81fe185ec74
Pete Bachant <petebachant@gmail.com>
2015-08-11 14:07:39 -0400

    Some work on getfigs script

--- getfigs.py
+++ getfigs.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
 """
-This script copies figures from the RVAT Reynolds number dependence
-experiment directory.
+This script copies figures into the local figures directory.
 """
 
 import os
 import sys
 import shutil
+from os.path import join
 
-figdir = os.path.join(os.path.expanduser("~"), "Research",
-                      "Experiments", "2014", "RVAT Re dep", "Figures")
+homedir = os.path.expanduser("~")
+expdir = join(homedir, "Research", "Experiments")
+foamrun_23x = join(homedir, "OpenFOAM", "pete-2.3.x", "run")
+foamrun_24x = foamrun_23x.replace("2.3.x", "2.4.x")
 
-figlist = ["cp_vs_tsr.pdf",
-           "mean_u.pdf",
-           "mean_upvp.pdf"]
+figdirs = {"RVAT-baseline": join(expdir, "RVAT baseline", "Figures"),
+           "RVAT-Re-dep": join(expdir, "RVAT Re dep", "Figures"),
+           "AD": join(foamrun_24x, "actuatorSurface", "figures")}
 
-for fig in figlist:
-    shutil.copy2(os.path.join(figdir, fig), os.path.join("figures", fig))
+figlists = {"RVAT-baseline": [],
+            "RVAT-Re-dep": ["cp_vs_tsr.pdf", "mean_u.pdf", "mean_upvp.pdf"],
+            "AD": ["AD_streamwise.pdf", "meancontquiv_AD.pdf"]}
+
+for name, figlist in figlists.items():
+    figdir = figdirs[name]
+    for fig in figlist:
+        oldfigpath = join(figdir, fig)
+        newfigpath = os.path.join("figures", name + "_" + fig)
+        if os.path.isfile(oldfigpath):
+            shutil.copy2(oldfigpath, newfigpath)
+            print("Copied {}".format(oldfigpath))
+        else:
+            print("{} does not exist".format(oldfigpath))
