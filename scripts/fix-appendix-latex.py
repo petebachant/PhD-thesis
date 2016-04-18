ef2d640c9fee92e01f2622dbd9477f1e10e1ecaf
Pete Bachant <petebachant@gmail.com>
2016-04-17 22:56:23 -0400

    Add RM2 dataset appendix

--- scripts/fix-appendix-latex.py
+++ scripts/fix-appendix-latex.py
@@ -15,7 +15,7 @@ def fix_image_url(line):
     The line will look something like this:
         \includegraphics{http://i.imgur.com/W5IycYU.jpg}
     """
-    url = line.strip().replace("\\includegraphics{", "").replace("}", "")
+    url = line.strip().split("\\includegraphics{")[-1].replace("}", "")
     if not os.path.isdir("figures/downloaded"):
         os.makedirs("figures/downloaded")
     fout = "figures/downloaded/" + url.strip().split("/")[-1]
@@ -29,21 +29,25 @@ def fix_image_url(line):
 
 def fix_file(fname):
     """Apply rules to given file."""
+    bad_kws = ["\\textbf{Figure", "Welcome", "\\tightlist"]
     with open(fname) as f:
         txt = ""
         for line in f.readlines():
             # line = "%r" % line
-            if "\\tightlist" in line or "\\textbf{Figure" in line:
+            if any(kw in line for kw in bad_kws):
                 line = ""
             elif "\\includegraphics" in line:
+                print("Got one", line)
                 line = fix_image_url(line)
+                print(line)
             txt += line
     with open(fname, "w") as f:
         f.write(txt)
 
 
 if __name__ == "__main__":
-    files = ["appendices/operation.tex"]
+    files = ["appendices/test-bed-wiki-operation.tex",
+             "appendices/rm2-wiki-home.tex"]
 
     for f in files:
         fix_file(f)
